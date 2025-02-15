618c8050bbe51974fbd925bb608d01e617a1f5c6
Fernandorsoul <fernnadorsoul@gmail.com>
2025-02-15 20:04:01 -0300

    Entregra da feature/#113(Create PersonDto)

---
+++ src/AgronomoPlus.Application/DTOs/PersonDto.cs
@@ -0,0 +1,10 @@
+namespace AgronomoPlus.Application.DTOs
+{
+    public class PersonDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
